29b2abd982c5b9483bf134d675b44ab995d9f5ad
iWDNN <cksgh2064@daum.net>
2022-10-25 01:28:54 +0900

    route

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
 const ajax = new XMLHttpRequest();
 const MEAL_URL = "https://www.themealdb.com/api/json/v1/1/search.php?f=a";
-const CONTENT_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=id";
+const CONTENT_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=@id";
 
 const container = document.getElementById("root");
 const ul = document.createElement("ul");
-const content = document.createElement("div");
 
-ajax.open("GET", MEAL_URL, false);
-ajax.send();
-const mealList = JSON.parse(ajax.response);
-
-window.addEventListener("hashchange", () => {
-  const id = location.hash.substring(1);
-
-  ajax.open("GET", CONTENT_URL.replace("id", id), false);
+function getData(url) {
+  ajax.open("GET", url, false);
   ajax.send();
+  return JSON.parse(ajax.response);
+}
 
-  const mealContent = JSON.parse(ajax.response);
+function mealFeed() {
+  const mealFeed = getData(MEAL_URL);
+  const mealList = [];
 
-  content.innerHTML = `
-    <h1>${mealContent.meals[0].strMeal}</h1>
-  `;
-});
-
-for (let i = 0; i < 4; i++) {
-  const div = document.createElement("li");
-
-  div.innerHTML = `
+  mealList.push("<ul>");
+  for (let i = 0; i < 4; i++) {
+    mealList.push(`
     <li>
-      <a href="#${mealList.meals[i].idMeal}">
-        ${mealList.meals[i].strMeal}
+      <a href="#${mealFeed.meals[i].idMeal}">
+        ${mealFeed.meals[i].strMeal}
       </a>
     </li>
-  `;
-  ul.appendChild(div.firstElementChild);
+  `);
+  }
+  mealList.push("</ul>");
+
+  container.innerHTML = mealList.join("");
 }
-container.append(ul);
-container.append(content);
+function mealDetail() {
+  const id = location.hash.substring(1);
+
+  const mealContent = getData(CONTENT_URL.replace("@id", id));
+
+  container.innerHTML = `
+    <h4><a href="#">목록으로</a></h4>
+    <h1>${mealContent.meals[0].strMeal}</h1>
+    <h3>area : ${mealContent.meals[0].strArea}</h3>
+    <h3>category${mealContent.meals[0].strCategory}</h3>
+    <img src="${mealContent.meals[0].strMealThumb}" />
+  `;
+}
+function router() {
+  const routePath = location.hash;
+  if (routePath === "") {
+    mealFeed();
+  } else {
+    mealDetail();
+  }
+}
+
+router();
+window.addEventListener("hashchange", router);
